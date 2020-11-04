576de07d080d2ed4fc989f3a5a7e201b93beeadd
arcticlimer <vinigm.nho@gmail.com>
2020-11-04 19:24:17 -0300

    🍱 Minor changes

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     //prevent "Confirm Form Resubmission" dialog boxes
     if (window.history.replaceState) {
         window.history.replaceState(null, null, window.location.href);
     }
 
     //search-bar
-    const searchForm = document.querySelector('#search-form');
-    const userInput = document.querySelector('#search-bar');
-    searchForm.addEventListener('submit', () => {
+    // TODO ajax
+    const searchForm = document.querySelector("#search-form");
+    const userInput = document.querySelector("#search-bar");
+    searchForm.addEventListener("submit", () => {
         event.preventDefault();
         window.location.href = `/user/${userInput.value}`;
     });
